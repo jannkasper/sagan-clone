8b1b52bbf1ff8f1646e8fc67717bb62585fd25c9
jannkasper <jann.kasper@gmail.com>
2021-06-21 07:17:40 +0200

    18 | UrlRewriteFilter

---
+++ sagan-site/src/main/java/sagan/site/UrlRewriterFilterConfig.java
@@ -0,0 +1,27 @@
+package sagan.site;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import sagan.site.support.rewrite.TuckeyRewriteFilter;
+
+@Configuration
+public class UrlRewriterFilterConfig {
+
+	public static final String REWRITE_FILTER_NAME = "rewriteFilter";
+	public static final String REWRITE_FILTER_CONF_PATH = "urlrewrite.xml";
+
+	
+	@Bean
+	public FilterRegistrationBean rewriteFilterConfig() {
+		FilterRegistrationBean reg = new FilterRegistrationBean();
+		reg.setName(REWRITE_FILTER_NAME);
+		reg.setFilter(new TuckeyRewriteFilter());
+		reg.addInitParameter("confPath", REWRITE_FILTER_CONF_PATH);
+		reg.addInitParameter("confReloadCheckInterval", "-1");
+		reg.addInitParameter("statusPath", "/redirect");
+		reg.addInitParameter("statusEnabledOnHosts", "*");
+		reg.addInitParameter("logLevel", "WARN");
+		return reg;
+	}
+}
